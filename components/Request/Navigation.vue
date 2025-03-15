1ec44f45421cedae80ec7bf7272dd0ecd394557e
starlingk21 <starlingk21@gmail.com>
2025-03-15 14:27:17 +0200

    feature(RequestPage): Navigation implemented

---
+++ components/Request/Navigation.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+	interface RequestNavigationItem {
+		url: string
+		name: string
+	}
+	
+	const options = ref([
+		{ url: '/', name: 'Contact us' },
+		{ url: '/', name: 'International offices and representatives'},
+		{ url: '/',  name: 'Items left on board' },
+		{ url: '/',  name: 'Compliment us' },
+		{ url: '/',  name: 'Ask a question' },
+		{ url: '/request',  name: 'Request assistance from us' },
+		{ url: '/',  name: 'Other inquiries' },
+		
+	])
+	
+	const route = useRoute()
+</script>
+
+<template>
+	<div class="request-nav w-81" role="navigation">
+		<ul class="nav-list rounded-md" role="list">
+			<li v-for="option in options"
+			    :class="['item px-6 py-3', { active: route.path === option.url }]"
+			    role="listitem">
+				<NuxtLink class="nav-link font-medium" :to="option.url">
+					{{ option.name }}
+				</NuxtLink>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	.request-nav {
+		max-width: 324px;
+		
+		.nav-list {
+			background-color: #f9f9f9;
+			
+			.item {
+				border-bottom: 1px solid #e2e4e9;
+				font-size: 14px;
+				line-height: 16px;
+				
+				&.active {
+					border-left: 2px solid #acb90c;
+				}
+				
+				&:last-child {
+					border-bottom: none;
+				}
+				
+				.nav-link {
+					color: #152649;
+					
+					&.router-link-active {
+						color: #acb90c;
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/request.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="request-page-wrapper flex mt-12" role="presentation">
+		<RequestNavigation />
+	</div>
+</template>
+
+<style scoped>
+
+</style>
